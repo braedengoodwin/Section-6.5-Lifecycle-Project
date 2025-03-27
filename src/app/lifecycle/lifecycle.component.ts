c5ae5c3559d1fee81b19f1c2974e4332c7e99e25
braedengoodwin <braedenagoodwin@hotmail.com>
2025-03-26 19:08:01 -0500

    finished lecture 124

---
+++ src/app/lifecycle/lifecycle.component.ts
@@ -0,0 +1,75 @@
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-lifecycle',
+  standalone: true,
+  imports: [],
+  templateUrl: './lifecycle.component.html',
+  styleUrl: './lifecycle.component.css',
+})
+export class LifecycleComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input() text?: string;
+
+  constructor() {
+    console.log('CONSTRUCTOR');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log(changes);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  // 'content' is any content that is projected into a 'view' using ngContent
+  // so the 'content' is some other (partial) 'view' data structure projected into this components view 
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  // 'view' in angular kind of refers to a components template (html file)
+  // technically the 'view' is an internally mangaged data structure that holds references to the dom elements rendered by a component 
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+}
